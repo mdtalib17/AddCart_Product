a6b2e32a0662f2a0ebed547e8fe6435161bd4b9b
Mohammad Talib <100046661+mdtalib17@users.noreply.github.com>
2023-04-18 03:49:20 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ProductCart from './components/ProductCart';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductCart/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ProductCart.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import ProductItem from './ProductItem';
+
+const ProductCart = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const handleAddToCart = (product) => {
+    setCartItems([...cartItems, product]);
+  };
+
+  const totalPrice = cartItems.reduce((acc, item) => acc + item.price, 0);
+
+  return (
+    <div className="product-cart">
+      <div className="product-list">
+        <ProductItem
+          product={{ name: 'Product 1', image: 'product1.jpg', price: 9.99 }}
+          onAddToCart={handleAddToCart}
+        />
+        <ProductItem
+          product={{ name: 'Product 2', image: 'product2.jpg', price: 19.99 }}
+          onAddToCart={handleAddToCart}
+        />
+        <ProductItem
+          product={{ name: 'Product 3', image: 'product3.jpg', price: 29.99 }}
+          onAddToCart={handleAddToCart}
+        />
+      </div>
+      <div className="cart-summary">
+        <h2>Cart Summary</h2>
+        <p>Total: {totalPrice.toFixed(2)}</p>
+        <ul>
+          {cartItems.map((item, index) => (
+            <li key={index}>
+              {item.name} - {item.price.toFixed(2)}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCart;
